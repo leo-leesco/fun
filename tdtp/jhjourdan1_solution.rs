8e0393dc1730d87058c4634414ca3d3a78bedee4
Jacques-Henri Jourdan <jacques-henri.jourdan@normalesup.org>
2022-02-07 13:57:46 +0100

    Update Hands-on.

--- tdtp/jhjourdan1_solution.rs
+++ tdtp/jhjourdan1_solution.rs
@@ -16,12 +16,14 @@ enum BST<K, V> {
 use BST::*;
 
 impl<K, V> BST<K, V> {
+    // Question 1
     fn new() -> Self {
         Leaf
     }
 }
 
 impl<K: Ord, V: Copy> BST<K, V> {
+    // Question 2
     fn find_copy_rec(&self, k: K) -> Option<V> {
         match self {
             Leaf => None,
@@ -35,6 +37,7 @@ impl<K: Ord, V: Copy> BST<K, V> {
         }
     }
 
+    // Question 3
     fn find_copy(&self, k: K) -> Option<V> {
         let mut tree = self;
         loop {
@@ -53,6 +56,7 @@ impl<K: Ord, V: Copy> BST<K, V> {
 
 
 impl<K: Ord, V> BST<K, V> {
+    // Not required by the statement
     fn find_rec<'a>(&'a self, k: K) -> Option<&'a V> {
         match self {
             Leaf => None,
@@ -65,6 +69,7 @@ impl<K: Ord, V> BST<K, V> {
         }
     }
 
+    // Question 4
     fn find<'a>(&'a self, k: K) -> Option<&'a V> {
         let mut tree = self;
         loop {
@@ -80,6 +85,7 @@ impl<K: Ord, V> BST<K, V> {
         }
     }
 
+    // Not required by the statement
     fn find_mut_rec<'a>(&'a mut self, k: K) -> Option<&'a mut V> {
         match self {
             Leaf => None,
@@ -92,6 +98,7 @@ impl<K: Ord, V> BST<K, V> {
         }
     }
 
+    // Not required by the statement
     fn find_mut<'a>(&'a mut self, k: K) -> Option<&'a mut V> {
         let mut tree = self;
         loop {
@@ -107,6 +114,7 @@ impl<K: Ord, V> BST<K, V> {
         }
     }
 
+    // Question 5
     fn insert(&mut self, k: K, v: V) -> Option<V> {
         let mut tree = self;
         loop {
@@ -126,14 +134,29 @@ impl<K: Ord, V> BST<K, V> {
     }
 }
 
-struct IntoIter<K, V> {
+struct IterMove<K, V> {
     cur: BST<K, V>,
     stack: Vec<(K, V, BST<K, V>)>
 }
 
-impl<K, V> Iterator for IntoIter<K, V> {
+impl<K, V> Iterator for IterMove<K, V> {
     type Item = (K, V);
     fn next(&mut self) -> Option<Self::Item> {
+        /* Here, we would actually like to write something like:
+               while let Node(b) = self.cur { ... }
+           and we would like to get full ownership of b.
+           However, Rust's ownership discipline makes this impossible, since
+           self is a borrow. Indeed, a borrow can never be "emptied", meaning it
+           has to always point to a value of the required type. This is
+           important because many Rust instructions can actually trigger a
+           panic (e.g., by stack overflow or out of memory), which may end a
+           borrow prematurely. If the borrow were emptied at that time, then
+           the lender could catch the panic and see an invalid value in the
+           borrowed variable.
+
+           To solve that issue, we use std::mem::replace in order to get the
+           full ownership of the content of self.cur, while leaving there a
+           default value (Leaf). */
         let mut cur = std::mem::replace(&mut self.cur, Leaf);
         while let Node(b) = cur {
             self.stack.push((b.key, b.val, b.right));
@@ -151,9 +174,9 @@ impl<K, V> Iterator for IntoIter<K, V> {
 
 impl<K, V> IntoIterator for BST<K, V> {
     type Item = (K, V);
-    type IntoIter = IntoIter<K, V>;
+    type IntoIter = IterMove<K, V>;
     fn into_iter(self) -> Self::IntoIter {
-        IntoIter{ cur: self, stack: vec![] }
+        IterMove{ cur: self, stack: vec![] }
     }
 }
 
@@ -165,6 +188,10 @@ struct Iter<'a, K, V> {
 impl<'a, K, V> Iterator for Iter<'a, K, V> {
     type Item = (&'a K, &'a V);
     fn next(&mut self) -> Option<Self::Item> {
+        /* Here, we do not need the std::mem::replace trick, because we only
+           need (and have) a share borrow, which is Copy. So b is actually a
+           shared borrow to the content of the Node, which can be extracted from
+           a borrow of the iterator, without making anything invalid. */
         while let Node(b) = self.cur {
             self.stack.push((&b.key, &b.val, &b.right));
             self.cur = &b.left
@@ -195,9 +222,29 @@ struct IterMut<'a, K, V> {
 impl<'a, K, V> Iterator for IterMut<'a, K, V> {
     type Item = (&'a K, &'a mut V);
     fn next(&mut self) -> Option<Self::Item> {
-        while let Some(Node(b)) = self.cur.take() {
+        /* We have the same problem as in IterMove: we would like to
+           temporarily take the content of self.cur, and keep the ownership of
+           self so that we can modify the field self.cur (in the match bellow).
+
+           However, we don't have a default value for &'a mut BST<K, V>, the
+           type of self.cur! Hence, the trick here is to use
+           Option<&'a mut BST<K, V>> for the type of self.cur, and to take
+           ownership of self.cur by replacing by None. This is exaclty what
+           self.cur.take() does: it is equivalent to
+           std::mem::replace(&mut self.cur, None). Then, unwrap() simply checks
+           that the returned value is of the form Some(x) and returns x. If it
+           is not, then it panics.
+
+           Note that the only performance penalty of this trick is the needless
+           write to self.cur and the check performed by unwrap. In particular,
+           Rust knows that Option<&'a mut BST<K, V>> can be stored in one word
+           only, using 0 for the representation of None (borrows can never be
+           NULL).
+         */
+        let mut cur = self.cur.take().unwrap();
+        while let Node(b) = cur {
             self.stack.push((&b.key, &mut b.val, &mut b.right));
-            self.cur = Some(&mut b.left)
+            cur = &mut b.left
         }
         match self.stack.pop() {
             None => None,
