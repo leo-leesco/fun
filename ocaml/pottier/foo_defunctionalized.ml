5207629bfe2b4ae9d7be43bdf0c793fbeb2d2245
François Pottier <francois.pottier@inria.fr>
2024-10-15 12:43:03 +0200

    Another update in the slides of week 5.

--- ocaml/pottier/foo_defunctionalized.ml
+++ ocaml/pottier/foo_defunctionalized.ml
@@ -1,7 +1,10 @@
+(* GADT *)
 type (_, _) closure =
   | NEqZero   : (int, bool) closure
   | Increment : (int,  int) closure
+(* GADT *)
 
+(* APPLY *)
 let rec apply =
   fun (type a b) (this : (a, b) closure) (that : a) : b ->
     match this with
@@ -11,23 +14,25 @@ let rec apply =
     | Increment ->
         let x = that in
         x + 1
+(* APPLY *)
 
+(* FILTERMAP *)
 let rec filter p xs =
   match xs with
-  | [] ->
-      []
-  | x :: xs ->
-      if apply p x then x :: filter p xs else filter p xs
+  | []      -> []
+  | x :: xs -> if apply p x then x :: filter p xs
+               else filter p xs
 
 let rec map f xs =
   match xs with
-  | [] ->
-      []
-  | x :: xs ->
-      apply f x :: map f xs
+  | []      -> []
+  | x :: xs -> apply f x :: map f xs
+(* FILTERMAP *)
 
+(* FOO *)
 let foo (xs : int list) : int list =
   filter NEqZero (map Increment xs)
+(* FOO *)
 
 let () =
   assert (foo [ -1; 0; +1 ] = [ 1; 2 ]);
